5a467567f1dd574509958a6175dac369d03f6971
Brian Graham <brian.graham@freighthub.com>
2018-05-28 13:44:36 +0200

    Improve test coverage

--- src/loader.spec.ts
+++ src/loader.spec.ts
@@ -6,14 +6,19 @@ describe('loader', () => {
         foo: loader.envGroup({
             BAR: types.NonEmptyString,
         }, 'FOO')
+        baz: loader.envGroup({
+            BAZ: types.NonEmptyString,
+        }, '')
     })
 
     describe('hydrateFromEnv', () => {
         it('should fill values from env object', () => {
             const env = loader.hydrateFrom({
                 FOO_BAR: 'test',
+                BAZ: 'test2',
             }, schema)
             expect(env.foo.BAR).toEqual('test')
+            expect(env.baz.BAZ).toEqual('test2')
         })
     })
 })
