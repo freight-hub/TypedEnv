c45b7118e9bf32e986c253b888c110440a904540
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2018-07-23 09:17:50 +0200

    Cover with specs

--- src/loader.spec.ts
+++ src/loader.spec.ts
@@ -8,10 +8,10 @@ describe('loader', () => {
         }, 'FOO'),
         baz: loader.envGroup({
             BAZ: types.NonEmptyString,
-        }, '')
+        }, ''),
     })
 
-    describe('LoadFromEnv', () => {
+    describe('#loadFrom', () => {
         it('should fill values from env object', () => {
             const env = loader.loadFrom({
                 FOO_BAR: 'test',
@@ -19,6 +19,25 @@ describe('loader', () => {
             }, schema)
             expect(env.foo.BAR).toEqual('test')
             expect(env.baz.BAZ).toEqual('test2')
+        })
+
+        it('reports errors when envvars are not set', () => {
+            expect(() => loader.loadFrom({ FOO_BAR: 2 }, schema))
+                .toThrowError(`Invalid value 2 supplied to : ENV/foo: FOO/BAR: NonEmptyString\nInvalid value undefined supplied to : ENV/baz: /BAZ: NonEmptyString`)
+        })
+    })
+
+    describe('#loadFromEnv', () => {
+        it('loads ennvars using process.env', () => {
+            const schema = loader.envSchema({
+                node: loader.envGroup({
+                    ENV: types.NonEmptyString,
+                }, 'NODE'),
+            })
+
+            const env = loader.loadFromEnv(schema)
+
+            expect(env).toEqual({ node: { ENV: 'test' } })
         })
     })
 })
